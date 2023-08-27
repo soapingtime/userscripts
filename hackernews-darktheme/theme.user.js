efcfac75c20f1bc43bc462167d792a78a17ddac7
soap <twinkd3ath@proton.me>
2023-08-27 13:03:51 -0500

    adblock

--- hackernews-darktheme/theme.user.js
+++ hackernews-darktheme/theme.user.js
@@ -24,3 +24,19 @@ function start() {
   var config = {
     textAreaBg: '#E0E0E0',
     textAreaLeftBorder: '12px solid #CCCCCC',
+
+// adblock, from https://github.com/brownie-in-motion/hn-adblock
+
+const FIX_NUMBERING = false;
+
+[...(document.querySelector('table.itemList')?.rows ?? [])]
+  .reduce((a, _r, i, rows) => (i % 3 ? a : [...a, rows.slice(i, i + 3)]), [])
+  .filter((row) => row.length === 3)
+  .filter(([title]) => !title.querySelector('td.votelinks'))
+  .forEach((rows) => rows.forEach((row) => row.remove()));
+
+if (FIX_NUMBERING) {
+  document
+    .querySelectorAll('span.rank')
+    .forEach?.((s, i) => s.textContent && (s.textContent = `${i + 1}.`));
+}

503effbba0242c399a250a7cb8392f294c22bfcd
soap <twinkd3ath@proton.me>
2023-09-08 06:59:36 -0500

    eruda fixes, add DOM

--- eruda/eruda.user.js
+++ eruda/eruda.user.js
@@ -4,11 +4,19 @@
 // @description   Console for mobile browsers
 // @copyright     2016-present, liriliri (https://github.com/liriliri)
 // @license       MIT
+// @version       1.0
 // @homepageURL   https://eruda.liriliri.io/
 // @require       https://cdn.jsdelivr.net/npm/eruda@latest/eruda.js
+// @require       https://cdn.jsdelivr.net/npm/eruda-code@latest/eruda-code.min.js
+// @require       https://raw.githack.com/uplusion23/eruda-dom/a5c4f12ef608f749effec2436e72ed5cc219df98/eruda-dom.min.js
 // @run-at        document-idle
 // @match         *://*/*
 // @noframes
 // ==/UserScript==
 
-eruda.init
+eruda.init({
+    tool:'console,elements,network,resources,sources,info,snippets'.split(',')
+});
+
+eruda.add(erudaDom);
+eruda.add(erudaCode)
